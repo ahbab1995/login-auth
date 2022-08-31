0c2de6b38ec3d07bff9271c0da698763887fd239
ahbab1995 <islamahbab1996@yahoo.com>
2022-08-31 21:41:42 +0600

    signup page google auth new code added

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,9 +1,25 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { auth } from "../firebase/Firebase.init";
 
 const Signup = () => {
+
+  const provider = new GoogleAuthProvider();
+
+  const handlegoogleAuth = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user)
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage)
+      });
+  };
   return (
-    <div>
+    <div className="py-4">
       <h2 className="text-center">Sign Up Form</h2>
       <div className="w-25 mx-auto">
         <Form>
@@ -32,7 +48,9 @@ const Signup = () => {
             Submit
           </Button>
         </Form>
-        
+        <div className="mt-3 d-flex justify-content-center">
+          <Button onClick={handlegoogleAuth}> Continue with Google</Button>
+        </div>
       </div>
     </div>
   );
